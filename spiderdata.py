07aa271530328081b3927d728ebd7cc58f0eb4f2
Jossey Batista <jossey.batista@gmail.com>
2022-03-01 17:29:49 -0500

    Seguimos con la misma prueba

--- spiderdata.py
+++ spiderdata.py
@@ -165,14 +165,15 @@ if (horario >= "00:00:01" and horario <= "23:59:00"):
                             draw.text((150+x, 790+y), "Tercer Premio",fill=color_a,font=font_h2)
                             draw.text((700+x, 770+y), tercer_premio,fill=color_a,font=font_h1)
 
-                            img.save("./saved-images/post_rs.png")
+                            img.save("./saved-images/post.png")
+                            img.save("./saved-images/post.jpg")
 
                             time.sleep(2)
 
-                            fb_rx = graph.put_object('102607489042095','photos',url='https://loteriapan.herokuapp.com/saved-images/post_rs.png',caption='Los ganadores 😀')
+                            fb_rx = graph.put_object('102607489042095','photos',url='https://loteriapan.herokuapp.com/saved-images/post.png',caption='Los ganadores 😀')
                             print(fb_rx)
 
-                            fb_rx_a = graph.put_object('17841452380183145','media',image_url='https://loteriapan.herokuapp.com/saved-images/post_rs.png',caption='es una prueba con foto y 😀')
+                            fb_rx_a = graph.put_object('17841452380183145','media',image_url='https://loteriapan.herokuapp.com/saved-images/post.jpg',caption='es una prueba con foto y 😀')
                             print(fb_rx_a)
 
                             fb_rx_b = graph.put_object('17841452380183145','media_publish',creation_id=fb_rx_a['id'],caption='es una prueba con foto y 😀')
