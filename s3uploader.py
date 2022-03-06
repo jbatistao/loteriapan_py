6e01470952df0acc9183c808b293ecfeece15e40
Jossey Batista <jossey.batista@gmail.com>
2022-03-06 00:55:19 -0500

    Prueba S3 - 4

--- s3uploader.py
+++ s3uploader.py
@@ -18,10 +18,10 @@ for bucket in s3.buckets.all():
 
 print("Enviando archivo...")
 
-ex = os.path.exists('.\saved-images\post.jpg')
+ex = os.path.exists('saved-images/post.jpg')
 
 if ex == True:
     # s3.Object('infoloteria','temp_bot.jpg').upload_file(r'.\source-images\temp_bot.jpg')
-    s3.Object('infoloteria','post20220302.jpg').upload_file('.\saved-images\post.jpg')
+    s3.Object('infoloteria','post20220302.jpg').upload_file('saved-images/post.jpg')
 
     print("Archivo cargado")
