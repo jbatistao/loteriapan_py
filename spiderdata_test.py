10e536acb5a8bb9b14a97810f64e97d1c83bdfe1
Jossey Batista <jossey.batista@gmail.com>
2022-03-26 17:37:41 -0500

    Se igualo el script con la version test que funciona 24 horas

--- spiderdata_test.py
+++ spiderdata_test.py
@@ -1,7 +1,5 @@
 # Spider 
 
-
-# from tkinter import image_names
 import firebase_admin
 from firebase_admin import credentials,db
 from firebase_admin import firestore
@@ -62,169 +60,168 @@ ahora = datetime.now()
 horario = ahora.strftime('%H:%M:%S')
 
 # Verifica si es hora de correr la consulta
-# if (horario >= "00: 00:01" and horario <= "23:59:00"):
+if (horario >= "00: 00:01" and horario <= "23:59:00"):
 # if (horario >= "13:45:00" and horario <= "14:45:00"):
 
-ano_hoy = ahora.strftime('%Y')
-mes_hoy = ahora.strftime('%m')
+    ano_hoy = ahora.strftime('%Y')
+    mes_hoy = ahora.strftime('%m')
 
-# Periodo que buscará
-anos = [ano_hoy]
-mes = [mes_hoy]
+    # Periodo que buscará
+    anos = [ano_hoy]
+    mes = [mes_hoy]
 
-# Recorrido
-ia = 0
-im = 0
-for a in anos:
-    # print('ia: ',ia)
+    # Recorrido
+    ia = 0
     im = 0
-    for m in mes:
-        url = 'http://www.lnb.gob.pa/numerosjugados.php?tiposorteo=T&ano='+anos[ia]+'&meses='+mes[im]+'&Consultar=Buscar'
-        r = requests.get(url)
-        soup = BeautifulSoup(r.content, 'html.parser')
-        data = soup.find_all('strong')
-    
-        # Verifica si hay datos de sorteos
-        if len(data) > 11:
+    for a in anos:
+        # print('ia: ',ia)
+        im = 0
+        for m in mes:
+            url = 'http://www.lnb.gob.pa/numerosjugados.php?tiposorteo=T&ano='+anos[ia]+'&meses='+mes[im]+'&Consultar=Buscar'
+            r = requests.get(url)
+            soup = BeautifulSoup(r.content, 'html.parser')
+            data = soup.find_all('strong')
+        
+            # Verifica si hay datos de sorteos
+            if len(data) > 11:
 
-            # Coloca los valores obtenidos por BS
-            i = 10
-            dias = ['Domingo','Lunes','Martes','Miércoles','Jueves','Viernes','Sábado']
-            meses = ['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre']
+                # Coloca los valores obtenidos por BS
+                i = 10
+                dias = ['Domingo','Lunes','Martes','Miércoles','Jueves','Viernes','Sábado']
+                meses = ['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre']
 
-            data_len = len(data)
-            for d in data:
-                if i < data_len:
-                    try:
-                        # Define campos según fecha del sorteo
-                        tipo_sorteo = data[i].text
-                        if tipo_sorteo == 'Intermedio':
-                            tipo_sorteo = 'Miercolito'
-                        if tipo_sorteo == 'Dominical':
-                            tipo_sorteo = 'Dominical'
-                        if tipo_sorteo == 'Zodiaco':
-                            tipo_sorteo = 'Gordito del Zodiaco'
-                        if tipo_sorteo == 'Extraordinaria':
-                            tipo_sorteo = 'Extraordinario'
+                data_len = len(data)
+                for d in data:
+                    if i < data_len:
+                        try:
+                            # Define campos según fecha del sorteo
+                            tipo_sorteo = data[i].text
+                            if tipo_sorteo == 'Intermedio':
+                                tipo_sorteo = 'Miercolito'
+                            if tipo_sorteo == 'Dominical':
+                                tipo_sorteo = 'Dominical'
+                            if tipo_sorteo == 'Zodiaco':
+                                tipo_sorteo = 'Gordito del Zodiaco'
+                            if tipo_sorteo == 'Extraordinaria':
+                                tipo_sorteo = 'Extraordinario'
 
-                        fecha_sorteo = datetime(int(data[i+1].text[-4:]),int(data[i+1].text[3:5]),int(data[i+1].text[:2]))
+                            fecha_sorteo = datetime(int(data[i+1].text[-4:]),int(data[i+1].text[3:5]),int(data[i+1].text[:2]))
 
-                        fecha_sorteo_cap = datetime(int(data[i+1].text[-4:]),int(data[i+1].text[3:5]),int(data[i+1].text[:2]))
-                        fecha_sorteo = str(fecha_sorteo.day) + ' de ' + meses[int(fecha_sorteo.strftime("%m"))-1] + ' de ' + fecha_sorteo.strftime("%Y")
-                        fecha_sort = fecha_sorteo_cap.strftime("%Y%m%d")
-                        primer_premio = data[i+2].text
-                        letras = data[i+3].text
-                        serie = int(data[i+4].text)
-                        folio = int(data[i+5].text)
-                        segundo_premio = data[i+6].text
-                        tercer_premio = data[i+7].text
-                        creado = ahora
+                            fecha_sorteo_cap = datetime(int(data[i+1].text[-4:]),int(data[i+1].text[3:5]),int(data[i+1].text[:2]))
+                            fecha_sorteo = str(fecha_sorteo.day) + ' de ' + meses[int(fecha_sorteo.strftime("%m"))-1] + ' de ' + fecha_sorteo.strftime("%Y")
+                            fecha_sort = fecha_sorteo_cap.strftime("%Y%m%d")
+                            primer_premio = data[i+2].text
+                            letras = data[i+3].text
+                            serie = int(data[i+4].text)
+                            folio = int(data[i+5].text)
+                            segundo_premio = data[i+6].text
+                            tercer_premio = data[i+7].text
+                            creado = ahora
 
-                        print(tipo_sorteo,fecha_sorteo,fecha_sorteo_cap,fecha_sort,primer_premio,letras,serie,folio,segundo_premio,tercer_premio)
-                        
-                        datasorteo = {
-                            'fecha_sorteo': fecha_sorteo,
-                            'fecha_sort': fecha_sort,
-                            'tipo_sorteo': tipo_sorteo,
-                            'primer_premio': primer_premio,
-                            'letras': letras,
-                            'serie': serie,
-                            'folio': folio,
-                            'segundo_premio': segundo_premio,
-                            'tercer_premio': tercer_premio,
-                            'creado': ahora
-                        }
-                        
-                        # Verifica los datos están completos
-                        if (tipo_sorteo != 'Gordito del Zodiaco' and len(tercer_premio) == 4) or (tipo_sorteo == 'Gordito del Zodiaco' and len(tercer_premio) == 2):
-                            # Verifica si el número y tipo de sorteo existe 
-                            docs = db.collection(u'sorteos').where(u'fecha_sort', u'==', fecha_sort).get()
-
-                        # Verifica si el número y tipo de sorteo existe
-                        if docs == []: 
-                            print(datetime.now(),' - No existe... debe guardarse')                             
-
-                            img = Image.open("./source-images/temp_bot.jpg")
-
-                            font_base = './fonts/Roboto-Medium.ttf'
-
-                            font_h1 = ImageFont.truetype(font_base,70)
-                            font_h2 = ImageFont.truetype(font_base,40)
-                            font_h3 = ImageFont.truetype(font_base,50)
-
-                            color_a = (255,255,255)
-                            x = 0
-                            y = 45
-                            draw = ImageDraw.Draw(img)
-                            draw.text((460+x, 0+y), "Sorteo",fill=color_a,font=font_h3)
-                            draw.text((380+x, 60+y), tipo_sorteo,fill=color_a,font=font_h1)
-                            draw.text((350+x, 150+y), fecha_sorteo,fill=color_a,font=font_h2)
-
-                            draw.text((150+x, 280+y), "Primer Premio",fill=color_a,font=font_h2)
-                            draw.text((700+x, 265+y), primer_premio,fill=color_a,font=font_h1)
-
-                            draw.text((200+x, 400+y), "Letras",fill=color_a,font=font_h2)
-                            draw.text((490+x, 400+y), "Serie",fill=color_a,font=font_h2)
-                            draw.text((770+x, 400+y), "Folio",fill=color_a,font=font_h2)
-
-                            draw.text((170+x, 450+y), letras,fill=color_a,font=font_h1)
-                            draw.text((500+x, 450+y), str(serie),fill=color_a,font=font_h1)
-                            draw.text((790+x, 450+y), str(folio),fill=color_a,font=font_h1)
-
-                            draw.text((150+x, 620+y), "Segundo Premio",fill=color_a,font=font_h2)
-                            draw.text((700+x, 600+y), segundo_premio,fill=color_a,font=font_h1)
-
-                            draw.text((150+x, 790+y), "Tercer Premio",fill=color_a,font=font_h2)
-                            draw.text((700+x, 770+y), tercer_premio,fill=color_a,font=font_h1)
-
-                            img_name = 'post'+fecha_sort+'.jpg'
-                            img_fullroute = 'saved-images/'+img_name
-
-                            img.save('./saved-images/'+img_name)
-                            print(datetime.now(),' - Imagen guardada')
-
-                            time.sleep(3)
+                            print(tipo_sorteo,fecha_sorteo,fecha_sorteo_cap,fecha_sort,primer_premio,letras,serie,folio,segundo_premio,tercer_premio)
                             
+                            datasorteo = {
+                                'fecha_sorteo': fecha_sorteo,
+                                'fecha_sort': fecha_sort,
+                                'tipo_sorteo': tipo_sorteo,
+                                'primer_premio': primer_premio,
+                                'letras': letras,
+                                'serie': serie,
+                                'folio': folio,
+                                'segundo_premio': segundo_premio,
+                                'tercer_premio': tercer_premio,
+                                'creado': ahora
+                            }
                             
-                            urls3 = 'https://infoloteria.s3.amazonaws.com/'+img_name
+                            # Verifica los datos están completos
+                            if (tipo_sorteo != 'Gordito del Zodiaco' and len(tercer_premio) == 4) or (tipo_sorteo == 'Gordito del Zodiaco' and len(tercer_premio) == 2):
+                                # Verifica si el número y tipo de sorteo existe 
+                                docs = db.collection(u'sorteos').where(u'fecha_sort', u'==', fecha_sort).get()
 
-                            # rr = os.path()
+                            # Verifica si el número y tipo de sorteo existe
+                            if docs == []: 
+                                print(datetime.now(),' - No existe... debe guardarse')                             
 
-                            s3.Object('infoloteria',img_name).upload_file(img_fullroute)
-                            # s3.Object('infoloteria',img_name).upload_file('saved-images/post.jpg')
-                            print(datetime.now(),' - Publicada en S3!')
-                            time.sleep(5)
+                                img = Image.open("./source-images/temp_bot.jpg")
 
-                            msg_regular = 'En el soteo ' + tipo_sorteo + ' de ' + fecha_sorteo + ' los números ganadores fueron: ' + '\n' + 'Primer Premio: ' + primer_premio + '\n' + 'Letras: ' + letras + '\n' + 'Serie: ' + str(serie) + '\n' + 'Folio: ' + str(folio) + '\n' + 'Segundo Premio: ' + segundo_premio + '\n' + 'Tercer Premio: ' + tercer_premio
+                                font_base = './fonts/Roboto-Medium.ttf'
 
-                            fb_rx = graph.put_object('102607489042095','photos',url=urls3,caption=msg_regular)
-                            print(datetime.now(),' - Publicada en FB!')
+                                font_h1 = ImageFont.truetype(font_base,70)
+                                font_h2 = ImageFont.truetype(font_base,40)
+                                font_h3 = ImageFont.truetype(font_base,50)
 
-                            fb_rx_a = graph.put_object('17841452380183145','media',image_url=urls3,caption=msg_regular)
-                            # print(fb_rx_a)
+                                color_a = (255,255,255)
+                                x = 0
+                                y = 45
+                                draw = ImageDraw.Draw(img)
+                                draw.text((460+x, 0+y), "Sorteo",fill=color_a,font=font_h3)
+                                draw.text((380+x, 60+y), tipo_sorteo,fill=color_a,font=font_h1)
+                                draw.text((350+x, 150+y), fecha_sorteo,fill=color_a,font=font_h2)
 
-                            fb_rx_b = graph.put_object('17841452380183145','media_publish',creation_id=fb_rx_a['id'])
-                            # print(fb_rx_b)
+                                draw.text((150+x, 280+y), "Primer Premio",fill=color_a,font=font_h2)
+                                draw.text((700+x, 265+y), primer_premio,fill=color_a,font=font_h1)
 
-                            print(datetime.now(),' - Publicada en IG!')
+                                draw.text((200+x, 400+y), "Letras",fill=color_a,font=font_h2)
+                                draw.text((490+x, 400+y), "Serie",fill=color_a,font=font_h2)
+                                draw.text((770+x, 400+y), "Folio",fill=color_a,font=font_h2)
 
-                            os.remove('./saved-images/'+img_name)
-                            print(datetime.now(),' - Se han borrado las imagenes!')
+                                draw.text((170+x, 450+y), letras,fill=color_a,font=font_h1)
+                                draw.text((500+x, 450+y), str(serie),fill=color_a,font=font_h1)
+                                draw.text((790+x, 450+y), str(folio),fill=color_a,font=font_h1)
+
+                                draw.text((150+x, 620+y), "Segundo Premio",fill=color_a,font=font_h2)
+                                draw.text((700+x, 600+y), segundo_premio,fill=color_a,font=font_h1)
+
+                                draw.text((150+x, 790+y), "Tercer Premio",fill=color_a,font=font_h2)
+                                draw.text((700+x, 770+y), tercer_premio,fill=color_a,font=font_h1)
+
+                                img_name = 'post'+fecha_sort+'.jpg'
+                                img_fullroute = 'saved-images/'+img_name
+
+                                img.save('./saved-images/'+img_name)
+                                print(datetime.now(),' - Imagen guardada')
+
+                                time.sleep(3)
                                 
-                            doc_ref = db.collection(u'sorteos').document()
-                            doc_ref.set(datasorteo)
-                            print(datetime.now(),' - Registro creado en Firebase')
-                        
-                        i = i+8
-                        # print('i: ',i)
-                    except IndexError:
-                        pass
-                    else:
-                        print(datetime.now(),' - No exception occurred')
+                                urls3 = 'https://infoloteria.s3.amazonaws.com/'+img_name
 
-            im = im+1
+                                s3.Object('infoloteria',img_name).upload_file(img_fullroute)
+                                print(datetime.now(),' - Publicada en S3!')
+                                time.sleep(5)
+
+                                msg_regular = 'En el soteo ' + tipo_sorteo + ' de ' + fecha_sorteo + ' los números ganadores fueron: ' + '\n' + 'Primer Premio: ' + primer_premio + '\n' + 'Letras: ' + letras + '\n' + 'Serie: ' + str(serie) + '\n' + 'Folio: ' + str(folio) + '\n' + 'Segundo Premio: ' + segundo_premio + '\n' + 'Tercer Premio: ' + tercer_premio
+
+                                fb_rx = graph.put_object('102607489042095','photos',url=urls3,caption=msg_regular)
+                                print(datetime.now(),' - Publicada en FB!')
+
+                                fb_rx_a = graph.put_object('17841452380183145','media',image_url=urls3,caption=msg_regular)
+                                # print(fb_rx_a)
+
+                                fb_rx_b = graph.put_object('17841452380183145','media_publish',creation_id=fb_rx_a['id'])
+                                # print(fb_rx_b)
+
+                                print(datetime.now(),' - Publicada en IG!')
+
+                                os.remove('./saved-images/'+img_name)
+                                print(datetime.now(),' - Se han borrado las imagenes!')
+
+                                 
+                                doc_ref = db.collection(u'sorteos').document()
+                                doc_ref.set(datasorteo)
+                                print(datetime.now(),' - Registro creado en Firebase')
+                            
+                            i = i+8
+                            # print('i: ',i)
+                        except IndexError:
+                            pass
+                        else:
+                            print(datetime.now(),' - No exception occurred')
+
+                im = im+1
+        
+        ia=ia+1
     
-    ia=ia+1
+    print(datetime.now(),' - No hay más datos')
 
-print(datetime.now(),' - No hay más datos')
-
+# else:
+#     print(datetime.now(),' - Estamos fuera de horario')    
