8c1f196c1dd6f14565efbd299dbecb9602af9781
Jossey Batista <jossey.batista@gmail.com>
2022-04-19 20:30:03 -0500

    Se prueba grabado en API

--- post.py
+++ post.py
@@ -1,4 +1,11 @@
 import requests
+import os
+from dotenv import load_dotenv
+
+dotenvvals = load_dotenv()
+
+key = os.getenv('API_KEY')
+
 pload = {
 		"fecha_sorteo": "2022-04-11",
 	    "fecha_sort": "20220411",
@@ -10,6 +17,9 @@ pload = {
 		"serie": 10,
 		"folio": 0
 	}
+
+headers_content = { "Authorization" : key }
+
 r = requests.post('https://notiloteria-api.herokuapp.com/api/sorteo/',json = pload)
 print(r.status_code)
 print(r.json())
